0a09af869d2e6ff200797dca42706e2d94fbb5aa
rahulstwn21 <rahulstwn@gmail.com>
2022-06-13 22:37:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/app_db_sqlite_rahulsetiawan/MataKuliah.kt
@@ -0,0 +1,8 @@
+package com.example.app_db_sqlite_rahulsetiawan
+
+class MataKuliah (
+    var kdMatkul : String,
+    var NmMatkul : String,
+    var sks : Int,
+    var sifat: String,
+        )
---
+++ app/src/main/java/com/example/app_db_sqlite_rahulsetiawan/layout_item.kt
@@ -0,0 +1,11 @@
+package com.example.app_db_sqlite_rahulsetiawan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_item)
+    }
+}
